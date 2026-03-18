d291e683ac1d02e69fb7dec5b19f265e07c15fa5
Lavanya B <lavanyab6293@gmail.com>
2026-03-18 21:29:16 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-//dotenv.config();
 dotenv.config();
 import cookieParser from "cookie-parser";
 
@@ -15,6 +15,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body) 
 app.use(cookieParser());
 
@@ -22,10 +24,20 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 // app.get("/", (req, res) => {
      //root route http://localhost:5000
 //     res.send("Hello world!");
 // });
+
+// app.get("/*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+app.use((req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
