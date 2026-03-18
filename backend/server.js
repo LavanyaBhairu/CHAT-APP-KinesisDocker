2755344d302d65c63630093eeeb50c56727dc174
Lavanya B <lavanyab6293@gmail.com>
2026-03-18 14:47:09 +0530

    commit changes

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,9 @@ import messageRoutes from "./routes/message.js";
 import userRoutes from "./routes/user.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
+//const app = express();
 
 const PORT = process.env.PORT || 5000;
 
@@ -26,7 +27,7 @@ app.use("/api/users", userRoutes);
 //     res.send("Hello world!");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`)
 });
