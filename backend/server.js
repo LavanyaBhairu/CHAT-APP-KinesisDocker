52f3f19b36d12cfeae0a70b06d345fa5dd5ed591
Lavanya B <lavanyab6293@gmail.com>
2026-03-18 13:04:45 +0530

    section 3 completed: connect ui to backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 //dotenv.config();
-dotenv.config({ path: "../.env" });
+dotenv.config();
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.js";
--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,27 @@
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
+
 const Message = ({ message }) => {
-	
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+	const shakeClass = message.shouldShake ? "shake" : "";
+
 	return (
-		<div className={`chat chat-end`}>
+		<div className={`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
 				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src={
-                        "https://www.google.com/imgres?q=avatar%20image%20url%20im%20.png%20format%20for%20react%20js&imgurl=https%3A%2F%2Fwww.w3schools.com%2Fhowto%2Fimg_avatar2.png&imgrefurl=https%3A%2F%2Fwww.w3schools.com%2Fhowto%2Fhowto_css_image_avatar.asp&docid=IN--qpeX1hje-M&tbnid=AhCy_cweM6lOWM&vet=12ahUKEwjp2bzPt6eTAxVV2DQHHb9YIo4QnPAOegQIIhAB..i&w=499&h=498&hcb=2&ved=2ahUKEwjp2bzPt6eTAxVV2DQHHb9YIo4QnPAOegQIIhAB"
-
-                    } />
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 				</div>
 			</div>
-			<div className={`chat-bubble text-white bg-blue-500`}>Hi what is up?</div>
-			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
 		</div>
 	);
 };
